e6a0c527e18509e5409df92aff67836b153c04ac
Andrii Navrotskiy <andrii.navrotskiy@uvoteam.com>
2020-08-04 12:40:49 +0300

    resolve di

--- app.php
+++ app.php
@@ -5,10 +5,14 @@ require __DIR__ . '/vendor/autoload.php';
 $containerBuilder = new DI\ContainerBuilder();
 $container = $containerBuilder
     ->addDefinitions([
-        \Task\Repository\BinListRepositoryInterface::class => \Task\Repository\BinListRepository::class,
-        \Task\Repository\CacheRepositoryInterface::class => \Task\Repository\CacheRepository::class,
-        \Task\Repository\ExchangeRateRepositoryInterface::class => \Task\Repository\ExchangeRateRepository::class,
-        \Psr\Log\LoggerInterface::class => \Task\Log\InlineLog::class,
+        \Task\Repository\BinListRepositoryInterface::class
+            => DI\get(\Task\Repository\BinListRepository::class),
+        \Task\Repository\CacheRepositoryInterface::class
+            => DI\get(\Task\Repository\CacheRepository::class),
+        \Task\Repository\ExchangeRateRepositoryInterface::class
+            => DI\get(\Task\Repository\ExchangeRateRepository::class),
+        \Psr\Log\LoggerInterface::class
+            => DI\get(\Task\Log\InlineLog::class),
     ])
     ->build();
 
